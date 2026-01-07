cd36910d864c3bfffa868c1af63ef0cbf01b57ed
Arthur França <arthur.fortes@yardex.com.br>
2026-01-07 11:07:55 -0300

    feature: adicionando rotas de registro de frete

--- app/Http/Controllers/FreightController.php
+++ app/Http/Controllers/FreightController.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\Freight\UseCases\RegisterFixedFreightUseCase;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use RegisterKmFreightUseCase;
+use StoreFixedFreightRequest;
 use StoreKmFreightRequest;
 
 class FreightController extends Controller
 {
-    public function storeKm(StoreKmFreightRequest $request, RegisterKmFreightUseCase $registerKm){
+    public function storeKm(StoreKmFreightRequest $request, RegisterKmFreightUseCase $registerKm): JsonResponse
+    {
         $freight = $registerKm->execute($request->validated());
-        return response()->json($freight, 201);
+        return response()->json([
+            'data' => $freight
+        ], 201);
+    }
+
+    public function storeFixed(
+        StoreFixedFreightRequest $request,
+        RegisterFixedFreightUseCase $registerFixed
+    ): JsonResponse {
+        $freight = $registerFixed->execute($request->validated());
+        return response()->json([
+            'data' => $freight
+        ], 201);
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'freights', 'as' => 'freights.'], function () {
+    Route::post('/storeKm', [\App\Http\Controllers\FreightController::class, 'storeKm'])->name('storeKm');
+    Route::post('/storeFixed', [\App\Http\Controllers\FreightController::class, 'storeFixed'])->name('storeFixed');
+});
